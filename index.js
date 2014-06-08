3a0dddba398a15da3c48add814039f5a41962b5c
Adam Magaluk <AdamMagaluK@gmail.com>
2014-06-08 10:25:11 -0400

    Bumped npm version, fixed beta arduino detection

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var arduino150 = require('./lib/platform_150');
 var arduino105 = require('./lib/platform_105');
 
 module.exports = function(runtime, build, board){
-  if(runtime.ide.version.indexOf('1.5') === 0) {
+
+  // test for new beta version or nightly builds
+  if(/^(1.5|nightly)/.test(runtime.ide.version)){
     return arduino150.apply(this, arguments);
   }else {
     return arduino105.apply(this, arguments);
