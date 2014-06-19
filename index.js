277f1cf008b3791a8215f61a6dea9c3c922a2cc8
Adam Magaluk <AdamMagaluK@gmail.com>
2014-06-19 15:22:21 -0400

    Exporting config into env. Now looking for version that look like 150 not 1.5.0

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var arduino105 = require('./lib/platform_105');
 module.exports = function(runtime, build, board){
 
   // test for new beta version or nightly builds
-  if(/^(1.5|nightly)/.test(runtime.ide.version)){
+  if(/^(15|nightly)/.test(runtime.ide.version)){
     return arduino150.apply(this, arguments);
   }else {
     return arduino105.apply(this, arguments);
